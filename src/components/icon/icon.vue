30ef11e4a7fddde66e7d7e5d885a1d88b1ea4c12
ziguangliunian <2313617448@qq.com>
2020-05-06 16:36:53 +0800

    Add: 新增Icon组件

---
+++ src/components/icon/icon.vue
@@ -0,0 +1,27 @@
+<template>
+    <i :class="['iconfont',name]" :style="{color,fontSize:size+'px'}"></i>
+</template>
+
+<script>
+  export default {
+    name: "terse-icon",
+    props: {
+      name: {
+        type: String,
+        required: true
+      },
+      color: {
+        type: String,
+        default: '#000'
+      },
+      size: {
+        type: [Number, String],
+        default: 16
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
