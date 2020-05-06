45690b6b09e0aca1d5def23a4be473ad81b7d434
ziguangliunian <2313617448@qq.com>
2020-05-06 16:44:16 +0800

    Add: 新增ButtonGroup组件

---
+++ src/components/buttonGroup/buttonGroup.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="li-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "terse-button-group"
+  }
+</script>
+
+<style scoped lang="less">
+    .li-button-group {
+        display: inline-flex;
+        vertical-align: middle;
+
+        .li-btn {
+            border-radius: 0;
+
+            &:not(:first-child) {
+                margin-left: -1px;
+            }
+
+            &:hover {
+                z-index: 1;
+            }
+
+            &:first-child {
+                border-top-left-radius: 4px;
+                border-bottom-left-radius: 4px;
+            }
+
+            &:last-child {
+                border-bottom-right-radius: 4px;
+                border-top-right-radius: 4px;
+            }
+        }
+
+    }
+</style>
